b23ac958f24f75de3a3d7712802ee66d4e15f64d
Fabzman <Fabian.est.figueroa@gmail.com>
2018-12-19 15:39:39 +1100

    Enemy health script
    created the script

---
+++ Magic Missle/Assets/Script/EnemyHit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHit : MonoBehaviour {
+
+
+    public int enemyhealth;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (enemyhealth <=0)
+        {
+            Destroy gameObject;
+        }
+	}
+}
