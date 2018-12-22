98f9256584d3803a310eb59bb1c181506c94e2b4
Fabzman <Fabian.est.figueroa@gmail.com>
2018-12-23 04:56:58 +1100

    Wizards!!!
    Enemy wizards now shoot and damage the player, shield is the last functionality

---
+++ Magic Missle/Assets/Script/EnemyFire.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFire : MonoBehaviour {
+
+    public float bulletSpeed;
+    public float lifetime;
+    public ParticleSystem fire;
+    private PlayerStats _player;
+
+    // Use this for initialization
+    void Start ()
+    {
+        _player = GameObject.Find("Player").GetComponent<PlayerStats>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(Vector3.forward * bulletSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Instantiate(fire.gameObject, transform.position, fire.transform.rotation);
+            _player.health -= 1;
+            Destroy(gameObject);
+        }
+
+        if (other.tag != "Enemy")
+        {
+            Destroy(gameObject);
+            Debug.Log(other.tag);
+        }
+    }
+}
--- Magic Missle/Assets/Script/EnemyShoot.cs
+++ Magic Missle/Assets/Script/EnemyShoot.cs
@@ -7,16 +7,31 @@ public class EnemyShoot : MonoBehaviour {
     private PlayerStats _player;
     public float shootSpeed;
     public Transform player;
+    public int magicDamage = 1;
+    public float fireRate = .25f;
+    public float magicRange = 50f;
+    private float nextFire;
+    public EnemyFire shot;
+    public Transform magicStart;
 
     // Use this for initialization
     void Start ()
     {
-		
-	}
+        _player = GameObject.Find("Player").GetComponent<PlayerStats>();
+        player = GameObject.Find("Player").transform;
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+        if (Vector3.Distance(transform.position, player.position) <= magicRange && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            EnemyFire newBullet = Instantiate(shot, magicStart.position, magicStart.rotation) as EnemyFire;
+            Vector3 look = player.position;
+            //look.y = newBullet.transform.position.y;
+            newBullet.transform.LookAt(look);
+        }
+
+    }
 }
